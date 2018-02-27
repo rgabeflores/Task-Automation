092ea3059ebd127f5912a50be174094bc159bd1d
rgabeflores <rgabeflores@gmail.com>
2018-02-27 00:09:10 -0800

    Added pandas & numpy

--- Python-Math/Exam-Statistics/stats.py
+++ Python-Math/Exam-Statistics/stats.py
@@ -1,8 +1,9 @@
+import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
-from statistics import mean, median, mode, stdev, variance
 
 '''
-	A basic script to graph test scores and calculate statistical data.
+    A basic script to graph test scores and calculate statistical data.
 '''
 
 
@@ -11,31 +12,46 @@ def get_scores():
     # Gets test scores from text file
     try:
         with open('scores.txt') as data:
-            values = [int(x.strip()) for x in data.readlines()]
+            return [int(x.strip()) for x in data.readlines()]
     except FileNotFoundError as e:
         from sys import exit
         print('scores.txt was not found.')
         exit()
-    else:
-        return values
 
 
 def output(values):
-        # Formats output
-    content = 'Mean: {}\nMedian: {}\nMode: {}\nVariance: {}\nStandard Deviation: {}'.format(
-        mean(values),
-        median(values),
-        mode(values),
-        variance(values),
-        stdev(values))
+    # Formats output
+    df = pd.DataFrame(pd.Series([
+        np.amin(values),
+        np.amax(values),
+        np.mean(values),
+        np.median(values),
+        np.var(values),
+        np.std(values)
+    ], [
+        'Min',
+        'Max',
+        'Mean',
+        'Median',
+        'Variance',
+        'Standard Deviation'
+    ]),
+        columns=[
+            'Exam 1'
+    ]
+    )
+
     print()
-    print(content)
-    # Writes output to 'results.txt'
+    print(df)
+    print()
+
     try:
+        # Writes output to 'results.txt'
         with open('results.txt', 'w') as output:
-            output.write(content)
-    except:
-        print('Something went wrong while writing to the file.')
+            output.write(df.to_string())
+    except Exception as e:
+        print(e)
+        # print('Something went wrong while writing to the file.')
 
 
 def main():
@@ -44,15 +60,15 @@ def main():
     output(values)
 
     # X-axis values
-    x = [i for i in range(min(values), max(values) + 1)]
+    x = np.array([i for i in range(min(values), max(values) + 1)])
     # Y-axis values
-    y = [0 for i in range(25)]
+    y = np.array([0 for i in range(25)])
 
     # Adds up score quantities
     for j in values:
         y[j] += 1
 
-    plt.bar(x, y[min(values):], label='Occurrences')
+    plt.bar(x, y[min(values):], label='Scores')
     plt.xlabel('Score')
     plt.ylabel('Quantity')
     plt.title('Exam 1 Scores')
