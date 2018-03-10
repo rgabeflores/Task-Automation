8ada3cb21ac671f049a0c359ccdb25190109621f
rgabeflores <rgabeflores@gmail.com>
2018-03-09 22:30:52 -0800

    Added Logging

--- Text-Comparison/textComparison.py
+++ Text-Comparison/textComparison.py
@@ -1,9 +1,14 @@
 import sys
-from difflib import ndiff
+import logging
+from difflib import ndiff, context_diff
+
 '''
     Compares text in two files. Made for testing generated output from student
     assignment submissions against expected output from correct code.
+
+    Prints results to screen and logs to file.
 '''
+logging.basicConfig(filename='results.log', level=logging.INFO)
 
 
 def main():
@@ -12,15 +17,17 @@ def main():
 
     for arg in sys.argv[1:]:
         try:
-            with open('./'.format(arg)) as f:
+            with open('./{}'.format(arg)) as f:
                 content.append(f.readlines())
         except FileNotFoundError as e:
-            sys.exit('One of the files is missing or misplaced.')
+            sys.exit('\nOne of the files is missing or misplaced.\n')
         except Exception as e:
-            sys.exit('Something went wrong.')
+            sys.exit('\nSomething went wrong.\n')
 
     results = ndiff(content[0], content[1])
-    print(''.join(results))
+    for result in results:
+        print(result)
+        logging.info(result)
 
 
 if __name__ == '__main__':
